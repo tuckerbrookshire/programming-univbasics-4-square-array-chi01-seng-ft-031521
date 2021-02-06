5c15a00eed5f877395a2ce0172e70d1219e92a3a
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-06 22:23:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,9 @@
-require 'pry'
-
 def square_array(array)
   counter = 0
   new_array = []
   
 while counter < array.length do  
-    new_array << (array[counter])*(array[counter])
+    new_array << array[counter]*array[counter];
     counter += 1
 end
   new_array
