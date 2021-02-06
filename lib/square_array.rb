407d873133c8113b8cdc61e39879c0f574f6da50
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-06 22:19:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,16 +5,9 @@ def square_array(array)
   new_array = []
   
 while counter < array.length do  
-    (array[counter])
+    (array[counter])**2
     counter += 1
 end
   new_array
 end
 
-def square_array(array)
-    new_array = []
-    array.each do |element|
-        new_array << element ** 2
-    end
-    new_array
-end
