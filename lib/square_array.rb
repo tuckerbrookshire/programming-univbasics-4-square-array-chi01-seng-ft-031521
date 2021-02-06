a900b61f2bfc8bd1a61ce92872b2490889bfe489
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-02-06 22:08:05 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def square_array(array)
-  # your code here
+  counter = 0
+  new_array = []
+  
+while counter < array.length do  
+    puts new_array.push(array[counter**])
+    binding.pry
+    counter += 1
+end
+  new_array
 end
